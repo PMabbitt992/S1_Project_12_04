79768581531800e718ff389556bbe1555f819ab1
Paige Mabbitt <PMabbitt992@west-mec.org>
2019-03-05 13:36:54 -0700

    Final Commit: Ready for Review
    function for party vote percent bars that changes color for each party and actual display of bars, comments

--- vw_results.js
+++ vw_results.js
@@ -38,8 +38,10 @@ function calcPercent(value, sum) {
     return (100 * value / sum);
 }
 
+// variable reportHTML equals an h1 heading with the race title inside
 var reportHTML = "<h1>" + raceTitle + "</h1>";
 
+//for loop that for each value in the race array, it will count up the amount of total votes and write the race name, and candidateRows (candidate votes, party, vote percentage, and total votes) to an HTML table
 for (var i = 0; i < race.length; i++) {
     var totalVotes = 0;
     votes[i].forEach(function calcSum(value) {
@@ -49,17 +51,40 @@ for (var i = 0; i < race.length; i++) {
     reportHTML += candidateRows([i], totalVotes);
     reportHTML += "</table>";
 }
-
+// input reportHTML to the section
 document.getElementsByTagName("section")[0].innerHTML = reportHTML;
 
+//function that returns the candidate name, party, and amount of votes and percent of votes for each candidate and array, then assigns it withing html code to be inserted into the html file
 function candidateRows(raceNum, totalVotes) {
-    var rowHTML = "";
+    var rowHTML = " ";
     for (var j = 0; j <= 2; j++) {
         var candidateName = candidate[raceNum][j];
         var candidateParty = party[raceNum][j];
         var candidateVotes = votes[raceNum][j];
         var candidatePercent = calcPercent(candidateVotes, totalVotes);
-        rowHTML += "<tr> <td>" + candidateName + "(" + candidateParty + ")</td> <td>" + candidateVotes.toLocaleString() + "(" + candidatePercent.toFixed(1) + "%)</td> </tr>";
-        return rowHTML;
+        rowHTML += "<tr> <td>" + candidateName + "(" + candidateParty + ")</td> <td>" + candidateVotes.toLocaleString() + "(" + candidatePercent.toFixed(1) + "%)</td>";
+        for (var k = 0; k < candidatePercent.toFixed(1); k++) {
+            rowHTML += createBar(candidateParty, candidatePercent);
+        }
+        rowHTML += "</tr>";
     }
+    return rowHTML;
+}
+
+// function that chooses a color for the bar base on the party of the candidate
+function createBar(partyType) {
+    var barHTML = " ";
+    switch (partyType) {
+        case partyType = "D":
+            barHTML = "<td class='dem'></td>"
+            break;
+        case partyType = "R":
+            barHTML = "<td class='rep'></td>"
+            break;
+        case partyType = "I":
+            barHTML = "<td class='ind'></td>"
+            break;
+    }
+    return barHTML;
+
 }
